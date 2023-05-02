2b424deb38b72322dc53b6f0901bf8702e13f9a9
HALIDIYA <aliyuhalimatsadiya93@gmail.com>
2023-05-02 13:49:13 -0700

    0x05. C - Pointers, arrays and strings project

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int-Swaps the values of 2 integers.
+ * @a:integer to swap
+ * @b:integer to swap
+ *
+ * Return:void.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int p = *a;
+	*a = *b;
+	*b = p;
+
+}
